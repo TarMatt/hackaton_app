dc59e77d57d849d5f72f92b550d3119b795c466e
Matteo <tarenzimatteo@gmail.com>
2025-04-13 14:36:43 +0200

    Added .gitignore

--- detection.py
+++ detection.py
@@ -1,10 +1,16 @@
 import requests
+import argparse
 
 backend_url = 'https://det-backendv2-497430069448.us-central1.run.app/predict/'
 
+parser = argparse.ArgumentParser(description="Pass the image file path")
+parser.add_argument('file_path', type=str, help="The file path to the image")
 
-with open('test_image.jpg', "rb") as img:
+args = parser.parse_args()
+path = args.file_path
+
+with open(path, "rb") as img:
     files = {"file": img}
     response = requests.post(backend_url, files=files)
 
-response.json()
+print(response.json())
